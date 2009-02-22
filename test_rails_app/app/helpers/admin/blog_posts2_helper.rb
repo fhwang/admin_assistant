9efb374b8a4827e506e003ac8c95baf83b320d3c
Francis Hwang <sera@fhwang.net>
2009-02-21 19:55:53 -0500

    #{column_name}_html_for_form methods can be defined in helper

--- test_rails_app/app/helpers/admin/blog_posts2_helper.rb
+++ test_rails_app/app/helpers/admin/blog_posts2_helper.rb
@@ -1,4 +1,10 @@
 module Admin::BlogPosts2Helper
+  def tags_html_for_form(blog_post)
+    text_field_tag(
+      'blog_post[tags]', blog_post.tags.map { |tag| tag.tag }.join(' ')
+    )
+  end
+  
   def tags_html_for_index(blog_post)
     blog_post.tags.map { |tag| tag.tag }.join ' '
   end
--- test_rails_app/spec/controllers/admin/blog_posts2_controller_spec.rb
+++ test_rails_app/spec/controllers/admin/blog_posts2_controller_spec.rb
@@ -2,6 +2,29 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Admin::BlogPosts2Controller do
   integrate_views
+  
+  describe '#edit' do
+    before :all do
+      BlogPost.destroy_all
+      @blog_post = BlogPost.create!(
+        :title => "blog post title", :body => 'blog post body'
+      )
+      tag1 = Tag.create! :tag => 'tag1'
+      BlogPostTag.create! :blog_post => @blog_post, :tag => tag1
+      tag2 = Tag.create! :tag => 'tag2'
+      BlogPostTag.create! :blog_post => @blog_post, :tag => tag2
+    end
+      
+    before :each do
+      get :edit, :id => @blog_post.id
+      response.should be_success
+    end
+    
+    it 'should show the tags' do
+      response.body.should match(%r|<input.*name="blog_post\[tags\]"|m)
+      response.body.should match(/(tag2 tag1|tag1 tag2)/)
+    end
+  end
 
   describe '#index' do
     describe 'when there is one record' do
