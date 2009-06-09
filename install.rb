6afcf643d3d6bfc62749c9b0b97c9c7bc14a86bb
Francis Hwang <sera@fhwang.net>
2009-06-09 08:55:53 -0400

    adding an img for the wiki

--- install.rb
+++ install.rb
@@ -1,2 +1,5 @@
-# Install hook code here
+# Removing directories that would be spammy if you were just using this as a 
+# plugin
+FileUtils.rm_rf "#{File.dirname(__FILE__)}/doc"
 FileUtils.rm_rf "#{File.dirname(__FILE__)}/test_rails_app"
+
