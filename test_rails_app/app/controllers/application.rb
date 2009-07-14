7da4d7c25fee3384ac81236dae62398588189b91
Francis Hwang <sera@fhwang.net>
2009-07-14 15:17:45 -0400

    don't use all helpers in test_rails_app's ApplicationController

--- test_rails_app/app/controllers/application.rb
+++ test_rails_app/app/controllers/application.rb
@@ -2,8 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '5d2981e5ebb4ecf32e79b347636948ae'
