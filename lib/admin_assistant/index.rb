e4ab4bbec74c29809d86087eb0ce7f4918046b64
Noah Davis <noahd1@yahoo.com>
2009-09-14 19:54:09 -0400

    make right column links more readable by separating with html bullet entity

--- lib/admin_assistant/index.rb
+++ lib/admin_assistant/index.rb
@@ -222,13 +222,14 @@ class AdminAssistant
       end
       
       def right_column_links(record)
-        links = ""
+        links = []
         links << edit_link(record) if render_edit_link?(record)
         links << delete_link(record) if render_delete_link?(record)
+
         if render_show_link?(record)
           links << @action_view.link_to(
             'Show', :action => 'show', :id => record.id
-          ) << ' '
+          )
         end
         right_column_lambdas.each do |lambda|
           link_args = lambda.call record
@@ -237,9 +238,9 @@ class AdminAssistant
         if @action_view.respond_to?(:extra_right_column_links_for_index)
           links << @action_view.extra_right_column_links_for_index(
             record
-          ) || ''
+          )
         end
-        links
+        links.join(" &bull; ")
       end
       
       def tr_css_classes(record)
