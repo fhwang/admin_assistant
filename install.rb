be921d6a89e316ca38e1bdd6af3b448f571667d5
Francis Hwang <francis.hwang@profitably.com>
2010-12-28 10:47:24 -0600

    admin_assistant's JS files belong in Rails.root/public/javascripts/admin_assistant/

--- install.rb
+++ install.rb
@@ -3,7 +3,7 @@ require 'pathname'
 
 outer = File.dirname(__FILE__)
 
-# Delete test_rails_app and various doc directories, unless you're actually
+# Delete rails_2_test and various doc directories, unless you're actually
 # developing admin_assistant itself
 test_rails_app = Pathname.new("#{outer}/rails_2_test").realpath.to_s
 unless RAILS_ROOT == test_rails_app
@@ -13,14 +13,9 @@ unless RAILS_ROOT == test_rails_app
 end
 
 # Copy over static assets
-css_dir = "#{RAILS_ROOT}/public/stylesheets/admin_assistant"
-FileUtils.mkdir(css_dir) unless File.exist?(css_dir)
-FileUtils.cp_r(Dir.glob("#{outer}/lib/stylesheets/*"), css_dir)
-FileUtils.copy(
-  "#{outer}/lib/javascripts/admin_assistant.js",
-  "#{RAILS_ROOT}/public/javascripts/admin_assistant.js"
-)
-images_dir = "#{RAILS_ROOT}/public/images/admin_assistant"
-FileUtils.mkdir(images_dir) unless File.exist?(images_dir)
-FileUtils.cp_r(Dir.glob("#{outer}/lib/images/*"), images_dir)
+%w(stylesheets javascripts images).each do |asset_type|
+  asset_dir = "#{Rails.root}/public/#{asset_type}/admin_assistant"
+  FileUtils.mkdir(asset_dir) unless File.exist?(asset_dir)
+  FileUtils.cp_r(Dir.glob("#{outer}/lib/#{asset_type}/*"), asset_dir)
+end
 
--- lib/admin_assistant/helper.rb
+++ lib/admin_assistant/helper.rb
@@ -2,8 +2,14 @@ class AdminAssistant
   module Helper
     def admin_assistant_includes(opts = {})
       theme = opts[:theme] || 'default'
-      stylesheet_link_tag("admin_assistant/#{theme}") +
-          javascript_include_tag('admin_assistant')
+      tags = stylesheet_link_tag("admin_assistant/#{theme}")
+      js_dir = Pathname.new(Rails.root) + "public/javascripts/admin_assistant"
+      Dir.entries(js_dir).each do |entry|
+        if entry =~ /\.js$/
+          tags << javascript_include_tag("admin_assistant/#{entry}")
+        end
+      end
+      tags
     end
   end
 end
