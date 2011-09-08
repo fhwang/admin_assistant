ef8f3687b1d34f3ef58e87528fd190a2637102bc
Greg Weber <greg@gregweber.info>
2011-09-08 14:36:15 -0700

    remove cruft related to Kaminari switch

--- lib/admin_assistant/index.rb
+++ lib/admin_assistant/index.rb
@@ -162,26 +162,6 @@ class AdminAssistant
           end
           scope = search.add_to_mongo_query scope
           scope.page(@index.url_params[:page].to_i).per settings.per_page 
-          #scope = scope.limit 
-          #scope = scope.offset($page * $per_page)
-          ##scope.paginate :page => @index.url_params[:page], :per_page => settings.per_page
-          #scope.instance_eval do
-            #def total_entries
-              #@total_entries ||= count
-            #end
-            #def total_pages
-              #@total_entries / $per_page
-            #end
-            #def current_page
-              #$page
-            #end
-            #def previous_page
-              #$page - 1
-            #end
-            #def next_page
-              #$page + 1
-            #end
-          #end
         else
           @ar_query = ARQuery.new(
             :order => order_sql, :include => find_include,
